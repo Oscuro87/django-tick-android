0d184af2252f7bac7115ea8790595bab2b5d8d41
Emmanuel Capelle <capelle.e@gmail.com>
2015-04-02 17:11:36 +0200

    Search sur la vue home

---
+++ app/src/main/java/org/ec/androidticket/activities/home/adapters/TicketHolder.java
@@ -0,0 +1,9 @@
+package org.ec.androidticket.activities.home.adapters;
+
+import android.widget.TextView;
+
+public class TicketHolder
+{
+    public TextView ticketCode;
+    public TextView status;
+}
